bf72645b1ca9be62273cb0efa4d8d6e5837d4e8e
Nick Wylynko <nick1014375@gmail.com>
2020-04-06 09:39:13 +0800

    basic game done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import styles from './style.module.css'
+import io from "socket.io-client";
 
-function App() {
+let socket;
+
+export default function App() {
+  const [board, setBoard] = useState()
+  const [status, setStatus] = useState("waiting for other player...");
+  const [connected, setConnected] = useState(false)
+
+  useEffect(() => {
+    console.log('render')
+  })
+
+  useEffect(() => {
+    socket = io('192.168.0.109:3001');
+    socket.on("connect", () => { setConnected(true); setStatus("waiting for other player..."); joinRoom(); console.log(socket.id) })
+    socket.on("disconnect", () => { setConnected(false); setStatus("not connected to server...") })
+
+    socket.on("status", setStatus);
+    socket.on("board", setBoard);
+
+  }, [])
+
+  function joinRoom() {
+    socket.emit("room", window.location.hash.replace("#", ''))
+    socket.on("room", room => window.location.hash = room)
+  }
+
+  useEffect(() => {
+    console.log(connected ? "connected" : "disconnected")
+  }, [connected])
+
+  function addCoin(y) {
+    socket.emit("addCoin", { y })
+  }
+
+  function Item({ value, y }) {
+    if (value === 0) {
+      return <div className={styles.empty} onClick={() => addCoin(y)} />
+    } else if (value === 1) {
+      return <div className={styles.red} onClick={() => addCoin(y)} />
+    } else if (value === 2) {
+      return <div className={styles.yellow} onClick={() => addCoin(y)} />
+    } else {
+      return null
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 style={connected ? { color: 'green' } : { color: 'red' }}>Connect 420 - {status}</h1>
+      {board ?
+        <div className={styles.container}>
+
+          {board.map(
+            (col, x) => col.map(
+              (row, y) => (
+                <Item key={"board" + x + y} value={row} y={y} />
+              )
+            )
+          )
+          }
+
+        </div> : <p>Loading...</p>
+      }
+
+    </>
   );
 }
 
-export default App;
+
+
