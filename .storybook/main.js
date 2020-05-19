df8814cb29d11fdf42bfdc5f291f8fc56c209754
Nick Wylynko <nick1014375@gmail.com>
2020-05-19 19:23:45 +0800

    add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  stories: ['../src/**/*.stories.ts'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+      },
+    },
+  ],
+};
---
+++ src/Components/Header/Header.stories.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+import Header from './Header';
+
+export default { title: 'Header' };
+
+// export const withText: () => JSX.Element = () => <Header roomID="test" subText={['test1', 'test2']} />;
